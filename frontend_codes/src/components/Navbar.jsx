d64144a3702610672f141a6d387e75729b6e4185
jashvanth370 <jashjazz11@gmail.com>
2025-06-22 22:54:38 +0530

    Authendication added for the login , logout and register section

--- frontend_codes/src/components/Navbar.jsx
+++ frontend_codes/src/components/Navbar.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import userApi from '../api/userApi';
 
 function Navbar() {
+
+  const isAuthendication = userApi.isAuthenticated();
+  const logout = userApi.logout;
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+        const isLogout = window.confirm('Are you sure you want to logout this user?');
+        if (isLogout) {
+            logout();
+            navigate('/');
+        }
+    };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
       <div className="container">
         <Link className="navbar-brand" to="/">ConnectHub</Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/profile/:id">Profile</Link></li>
+            {!isAuthendication && <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>}
+            {!isAuthendication && <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>}
+            {isAuthendication && <li className="nav-item"><Link className="nav-link" to="/profile/:id">Profile</Link></li>}
+            {isAuthendication && <li className="nav-item">
+              <button className="btn btn-link nav-link" onClick={handleLogout}>
+                Logout
+                </button>
+              </li>
+            }
+
+            
           </ul>
         </div>
       </div>
