fd585dafdd60a7c21f40a0006b874b95db0ba120
jashvanth370 <jashjazz11@gmail.com>
2025-06-25 12:19:46 +0530

    Comment added frontend

--- frontend_codes/src/components/PostCard.jsx
+++ frontend_codes/src/components/PostCard.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import postApi from '../api/postApi';
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from 'react-router-dom';
 
 function PostCard({ post, onLike }) {
+
+  const [userId, setUserId] = useState(null);
+  const navigate = useNavigate();
+
+  const [commentText, setCommentText] = useState("");
+  const [showCommentBox, setShowCommentBox] = useState(false);
+
+  const handleCommentSubmit = async (e) => {
+    e.preventDefault();
+    if (commentText.trim()) {
+      const res = await postApi.CommentPost(post._id, commentText);
+      console.log("Updated Post with Comment:", res.post);
+      setCommentText(""); // Clear after post
+      setShowCommentBox(false); // Optional: hide input after submitting
+      onLike(); // Refresh
+    }
+  };
+
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const decoded = jwtDecode(token);
+        const userId = decoded.userId || decoded._id;
+        if (userId) {
+          setUserId(userId);
+        } else {
+          console.error("User ID not found in token");
+        }
+      } catch (err) {
+        console.error("Invalid token", err);
+      }
+    } else {
+      console.warn("No token found in localStorage");
+    }
+  }, []);
 
   const handleLike = async (e) => {
     e.preventDefault();
@@ -10,13 +49,24 @@ function PostCard({ post, onLike }) {
     onLike();
   };
 
-  const handleComment = async (e) => {
-    e.preventDefault();
-    const text = prompt("Enter your comment:");
-    if (text?.trim()) {
-      const res = await postApi.CommentPost(post._id, text);
-      console.log("Updated Post with Comment:", res.post);
-      onLike();
+  // const handleComment = async (e) => {
+  //   e.preventDefault();
+  //   const text = prompt("Enter your comment:");
+  //   if (text?.trim()) {
+  //     const res = await postApi.CommentPost(post._id, text);
+  //     console.log("Updated Post with Comment:", res.post);
+  //     onLike();
+  //   }
+  // }
+
+  const handleDelete = async (postId) => {
+    try {
+      const res = await postApi.deletePost(postId, userId);
+      window.confirm("Do you want delete this post ... ? ");
+      navigate('/profile/:id');
+      return res;
+    } catch (error) {
+      console.error("error :", error)
     }
   }
 
@@ -37,13 +87,32 @@ function PostCard({ post, onLike }) {
           />
         )}
         <div>
-          
+
           <button className="btn btn-outline-primary btn-sm me-2" onClick={handleLike}>
             like {post.likes?.length}
           </button>
-          <button className="btn btn-outline-primary btn-sm me-2" onClick={handleComment}>
+          {/* {<button className="btn btn-outline-primary btn-sm me-2" onClick={handleComment}>
             comment  {post.comments?.length || 0}
-          </button>
+          </button>} */}
+
+          {showCommentBox && (
+            <form onSubmit={handleCommentSubmit}>
+              <input
+                className=" btn-outline-dark btn-lg m-2"
+                type="text"
+                placeholder="Write a comment..."
+                value={commentText}
+                onChange={(e) => setCommentText(e.target.value)}
+              />
+              <button className="btn btn-outline-primary btn-sm m-2" type="submit">Post</button>
+            </form>
+          )}
+          <button className="btn btn-outline-primary btn-sm me-2" onClick={() => setShowCommentBox(!showCommentBox)}>Comment</button>
+
+
+          {post.author?._id == userId && <button className="btn btn-outline-primary btn-sm me-2" onClick={() => handleDelete(post._id)}>
+            delete
+          </button>}
 
 
           {post.comments?.length > 0 && (
