68e295df66d8b2356dc471fbec6afb522c224e95
jashvanth370 <jashjazz11@gmail.com>
2025-07-07 11:15:48 +0530

    Url change and navbar footer added

--- frontend_codes/src/App.js
+++ frontend_codes/src/App.js
@@ -13,20 +13,20 @@ function App() {
   return (
     <Router>
       <div className="d-flex flex-column min-vh-100">
+        <Navbar />
         <main className="flex-grow-1">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/create-post' element={<CreatePost />} />
-          <Route path='/post-card' element={<PostCard />} />
-          <Route path='/feed-page' element={<FeedPage />} />
-          <Route path='/profile/:id' element={<UserProfile />} />
-          
-        </Routes>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/create-post' element={<CreatePost />} />
+            <Route path='/post-card' element={<PostCard />} />
+            <Route path='/feed-page' element={<FeedPage />} />
+            <Route path='/profile/:id' element={<UserProfile />} />
+          </Routes>
         </main>
+        <Footer />
       </div>
-      <Footer />
     </Router>
   );
 }
